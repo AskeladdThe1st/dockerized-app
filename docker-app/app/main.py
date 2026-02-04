2374511a4dc81d8e994acdc2f3ebc1e64a4d7bef
Yusuf <yusufrehmanit@gmail.com>
2026-02-04 03:01:03 -0800

    added a database function in main

--- docker-app/app/main.py
+++ docker-app/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from typing import Union
+import os
+import psycopg2
+
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"Hello": "Muhammad!"}
+    return {"status": "ok"}
 
-@app.get("/items/{item_id}")
-def read_items(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q" : q}
+@app.get("/db")
+def db_check():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    cur.close()
+    conn.close()
+    return {"db": "connected"}
