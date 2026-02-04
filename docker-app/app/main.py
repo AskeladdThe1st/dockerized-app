cc7bf5fd86360ab017282307d89f65b14b3ca71e
Yusuf <yusufrehmanit@gmail.com>
2026-02-04 05:00:05 -0800

    switched to os.environ for easier debugging because of silient fails and revamped dockerfile

--- docker-app/app/main.py
+++ docker-app/app/main.py
@@ -11,12 +11,17 @@ def root():
 
 @app.get("/db")
 def db_check():
+    required = ["DB_HOST","DB_PORT","DB_NAME","DB_USER","DB_PASSWORD"]
+    missing = [k for k in required if not os.getenv(k)]
+    if missing:
+        return {"error": f"Missing env vars: {missing}"}
+
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
     )
     cur = conn.cursor()
     cur.execute("SELECT 1;")
